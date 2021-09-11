4965da7c9cb5270a75835168b34e562c4de637c4
Jasmine <jasmine.quah@thoughtworks.com>
2021-09-11 13:56:59 +0800

    [JQ|Phung] Override to string method for Price value object, move ProductType to Product file

--- src/main/kotlin/com/thoughtworks/ddd/bootcamp/domain/product/Product.kt
+++ src/main/kotlin/com/thoughtworks/ddd/bootcamp/domain/product/Product.kt
@@ -4,4 +4,10 @@ import java.util.Currency
 
 data class Product(val type: ProductType, val price: Price)
 
-data class Price(val value: Double, val currency: Currency = Currency.getInstance("USD"))
+data class Price(val value: Double, val currency: Currency = Currency.getInstance("USD")) {
+    override fun toString(): String = "\$${String.format("%.2f", value)} $currency"
+}
+
+enum class ProductType {
+    IPAD_PRO, HERO_INK_PEN, GM_CRICKET_BAT
+}
--- src/main/kotlin/com/thoughtworks/ddd/bootcamp/domain/product/ProductType.kt
+++
@@ -1,5 +0,0 @@
-package com.thoughtworks.ddd.bootcamp.domain.product
-
-enum class ProductType {
-    IPAD_PRO, HERO_INK_PEN, GM_CRICKET_BAT
-}
